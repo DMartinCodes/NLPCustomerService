1d6ac66dc03ca86e1b439bd5138abee21e2efee9
James Mata <jamesmata556@gmail.com>
2023-05-06 15:32:42 -0400

    server starting point

--- Server/clientInput.py
+++
@@ -1,7 +0,0 @@
-# Import the CGI module
-import cgi
-# Get the value of the question parameter from the HTTP POST request
-form = cgi.FieldStorage() # instantiate only once!
-question = form.getvalue('question')
-# Print the value of the question parameter to the console
-print(question)
